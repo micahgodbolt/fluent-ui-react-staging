6b35cd302318193af606f2ef63150916da0ddf39
Ken Chau <kchau@microsoft.com>
2019-10-18 17:28:05 -0700

    added some jsdoc for doc gen

--- packages/base/src/components/Slider/Slider.props.ts
+++ packages/base/src/components/Slider/Slider.props.ts
@@ -10,16 +10,33 @@ export interface SliderSlotProps {
 }
 
 export interface SliderProps {
+  /** min value */
   min?: number;
+
+  /** max value */
   max?: number;
+
+  /** step */
   step?: number;
+
+  /** should snap to step */
   snapToStep?: boolean;
+
+  /** whether this is vertical or not */
   vertical?: boolean;
+
+  /** current value (controlled) */
   value?: number;
+
+  /** default value (uncontrolled) */
   defaultValue?: number;
 
+  /** on change handler (controlled) */
   onChange?: (ev: MouseEvent | KeyboardEvent, value: number) => void;
 
+  /** slot */
   slots?: any;
+
+  /** slot props */
   slotProps?: SliderSlotProps;
 }
